4a0660a14ee2d659dceaa3e85b383c5f0decdf0e
Kurkos <k57ae05v@inbox.lv>
2019-02-18 17:47:29 +0200

    k

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import Header from './components/Header/Header';
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">React App</div>;
+    return (
+    <React.Fragment>
+      <div className="App">React App</div>
+      <Header/>
+    </React.Fragment>
+    )
   }
 }
 
