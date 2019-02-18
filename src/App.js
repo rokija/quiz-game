349ef23b30ca757e2c343b157d881a28970e71ea
Student <Student@LVAGPLTP0823_BC.dir.svc.accenture.com>
2019-02-18 18:00:59 +0200

    DESCRIPTION Layout form building

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from "./components/Layout/Layout"
 class App extends React.Component {
  render() {
    return <div className="App">
-     <div className=".col">
+     <div className="">
      <Layout/>
     
      </div>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,57 @@
 import React, { Component } from "react";
 import   "./Layout.css"
-import Registration from "../Registration/Registration"
+// import Registration from "../Registration/Registration"
+import { Container, Row, Col } from 'reactstrap';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
  class Layout extends Component {
    render() {
-     return (
-       <div className="Layout">
-       Test
-       </div>
+     return(
+       
+       <Container>
+        <Row className="headerContent">
+          <Col>Header content goes there</Col>
+        </Row>
+       
+      
+      
+          <div className="blockBackground">
+            <Card body className="paddingBottom">
+            <Row > 
+            <Col xs="auto" className="positioning">
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+              <Button>Go somewhere</Button>
+              </Col>
+              </Row>
+            </Card>
+            <Card body className="paddingBottom">
+            <Row>
+            <Col xs="auto" className="positioning">
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+              <Button>Go somewhere</Button>
+              </Col>
+              </Row>
+              
+            </Card>
+
+            <Card body className="paddingBottom">
+            <Row>
+            <Col xs="auto" className="positioning">
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+              <Button>Go somewhere</Button>
+              </Col>
+              </Row>
+              
+            </Card>
+            </div>
+
+            <Row className="footerContent">
+          <Col>Footer content goes here</Col>
+        </Row>
+            </Container>
 
      );
    }
