80f5a30fee9db13c67bbf93fcad0100f04cd9919
Student <Student@LVAGPLTP0823_BC.dir.svc.accenture.com>
2019-02-18 17:10:59 +0200

    DESCRIPTION Layout creating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
+import Layout from "./components/Layout/Layout"
+// import Registration from "./components/Registration/Registration"
 
-class App extends Component {
-  render() {
-    return <div className="App">React App</div>;
-  }
+class App extends React.Component {
+ render() {
+   return <div className="App">
+     <div className=".col">
+     <Layout/>
+    
+     </div>
+
+   </div>;
+ }
 }
 
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import   "./Layout.css"
+import Registration from "../Registration/Registration"
+
+ class Layout extends Component {
+   render() {
+     return (
+       <div className="Layout">
+       Test
+       </div>
+
+     );
+   }
+ }
+
+export default Layout;
