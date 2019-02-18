5d51f0d896e087135fb8d660223963058d751c40
Student <Student@LVAGPLTP0850_BC.dir.svc.accenture.com>
2019-02-18 16:52:59 +0200

    Created Login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import "./Login.css";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  onInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div className="Login">
+        <div className="Login__content">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                onChange={this.onInputChange}
+                className="form-control"
+                type="email"
+                name="email"
+                value={email}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                onChange={this.onInputChange}
+                className="form-control"
+                type="password"
+                name="password"
+                value={password}
+              />
+            </div>
+            <div className="buttons">
+            <Button className="Login-button" color="dark">
+              Login
+            </Button>
+
+            <Button color="dark">
+              Register
+            </Button>
+          
+            </div>
+       
+          </form>
+
+         </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
