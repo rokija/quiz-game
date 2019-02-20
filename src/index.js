3a9d780dbb8ae96f315fff57fa21211369450430
Student <Student@LVAGPLTP0828_BC.dir.svc.accenture.com>
2019-02-20 13:04:41 +0200

    DESCRIPTION: added index.js COMMENTS:

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import Login from "./components/Login/Login";
 import Results from './components/Results/Results'
 import QuestionList from "./components/Results/QuestionList/QuestionList";
 import * as serviceWorker from "./serviceWorker";
+import UserList from "./components/UserList/UserList";
+import Quizzes from "./components/Quizzes/Quizzes";
 
 class Root extends Component {
   render() {
@@ -18,6 +20,8 @@ class Root extends Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/results" component={Results} />
           <Route exact path="/results/questions" component={QuestionList} />
+          <Route exact path="/users" component={UserList} />
+          <Route exact path="/quizzes" component={Quizzes} />
         </Switch>
       </BrowserRouter>
     );
