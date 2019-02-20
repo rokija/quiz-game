117583867b40e7a8e20c140c2741fe37f984646d
Kurkos <k57ae05v@inbox.lv>
2019-02-20 13:09:37 +0200

    -

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import Login from "./components/Login/Login";
-<<<<<<< HEAD
 import QuizzContainer from "./containers/QuizzContainer";
-=======
 import UserList from "./components/UserList/UserList";
 import Quizzes from "./components/Quizzes/Quizzes";
->>>>>>> 43014e16bc0b2e15ec6e2906e00fba4b1188ffb7
 
 class Root extends Component {
   render() {
@@ -19,12 +16,9 @@ class Root extends Component {
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/login" component={Login} />
-<<<<<<< HEAD
           <Route exact path="/quizzes/create" component={QuizzContainer} />
-=======
           <Route exact path="/users" component={UserList} />
           <Route exact path="/quizzes" component={Quizzes} />
->>>>>>> 43014e16bc0b2e15ec6e2906e00fba4b1188ffb7
         </Switch>
       </BrowserRouter>
     );
