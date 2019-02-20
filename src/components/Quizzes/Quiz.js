c95d635f9ed6f25e5697ba0f43b48092251654ba
Elena <kellydj.official@gmail.com>
2019-02-20 10:37:49 +0200

    Fixed tabulation and created an array for quizzes

--- src/components/Quizzes/Quiz.js
+++ src/components/Quizzes/Quiz.js
@@ -1,36 +1,51 @@
 import React, { Component } from "react";
 import "./Quiz.css";
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 
 class Quiz extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            showContent:false,
-         }
-    }
+    this.state = {
+      showContent: false
+    };
+  }
 
-    toggleContent = () => this.setState({ showContent: !this.state.showContent });
+  toggleContent = () => this.setState({ showContent: !this.state.showContent });
 
-    render() {
-        const { title, description, isAdmin } = this.props;
-        const { showContent } = this.state;
+  render() {
+    const { title, description, isAdmin } = this.props;
+    const { showContent } = this.state;
 
-      return (
-          <div className="Quiz "> 
-           <h1 onClick={this.toggleContent} className="Quiz__header ">{title}</h1>
-            {showContent && 
-              <div className="Quiz__content">
-            <div className="Quiz__content__description">{description }</div>
-              {isAdmin && <div  className="Quiz__content__buttons">
-               <Button color="secondary" className="Quiz__content__buttons__edit-button">Edit quiz</Button>
-               <Button color="secondary">Add question</Button></div>} 
-              {!isAdmin && <div className="Quiz__content__buttons">
-               <Button color="secondary">Start quiz</Button></div>}
-             </div>}
+    return (
+      <div className="Quiz ">
+        <h1 onClick={this.toggleContent} className="Quiz__header ">
+          {title}
+        </h1>
+        {showContent && (
+          <div className="Quiz__content">
+            <div className="Quiz__content__description">{description}</div>
+            {isAdmin && (
+              <div className="Quiz__content__buttons">
+                <Button
+                  color="secondary"
+                  className="Quiz__content__buttons__edit-button"
+                >
+                  Edit quiz
+                </Button>
+                <Button color="secondary">Add question</Button>
+              </div>
+            )}
+            {!isAdmin && (
+              <div className="Quiz__content__buttons">
+                <Button color="secondary">Start quiz</Button>
+              </div>
+            )}
           </div>
-      )}
-    }
+        )}
+      </div>
+    );
+  }
+}
 
-    export default Quiz;
+export default Quiz;
--- src/components/Quizzes/Quizzes.js
+++ src/components/Quizzes/Quizzes.js
@@ -2,19 +2,47 @@ import React, { Component } from "react";
 import Quiz from "./Quiz";
 import "./Quizzes.css";
 
+const quizzes = [
+  {
+    title: "First Quiz",
+    description: "Description about quiz",
+    isAdmin: false
+  },
+  {
+    title: "Second Quiz",
+    description: "Description about quiz",
+    isAdmin: true
+  },
+  {
+    title: "Third Quiz",
+    description: "Description about quiz",
+    isAdmin: false
+  },
+  {
+    title: "Fourth Quiz",
+    description: "Description about quiz",
+    isAdmin: true
+  },
+  { title: "Fifth Quiz", description: "Description about quiz", isAdmin: false }
+];
+
 class Quizzes extends Component {
-    render() {
-        return (
-            <div className="Quizzes">
-                <div className="Quizzes__content">
-                    <Quiz title="First Quiz" description="Description about quiz" isAdmin={false}/>
-                    <Quiz title="Second Quiz" description="Description about quiz" isAdmin={true}/> 
-                    <Quiz title="Third Quiz" description="Description about quiz" isAdmin={false}/>
-                    <Quiz title="Fourth Quiz" description="Description about quiz" isAdmin={true}/> 
-                    <Quiz title="Fifth Quiz" description="Description about quiz" isAdmin={false}/>
-                </div>
-            </div>   
-         )}
+  render() {
+    return (
+      <div className="Quizzes">
+        <div className="Quizzes__content">
+          {quizzes.map((quiz, i) => (
+            <Quiz
+              key={i}
+              title={quiz.title}
+              description={quiz.description}
+              isAdmin={quiz.isAdmin}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Quizzes
+export default Quizzes;
