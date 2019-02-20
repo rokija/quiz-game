ec281f9e792878b142f473011a248aea2512660b
Kurkos <k57ae05v@inbox.lv>
2019-02-20 12:21:42 +0200

    REFERENCE:Container components & rout paths; DESCRIPTION:Added Container components and rout paths;

--- src/index.js
+++ src/index.js
@@ -11,22 +11,23 @@ class Root extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={App} />
+          <Route exact path="/quizzes/:quizId/edit/questions/:questionId" component={App} />
+          <Route exact path="/quizzes/:quizId/edit/questions/create" component={App} />
+          <Route exact path="/quizzes/:quizId/edit/questions" component={App} />
+          <Route exact path="/quizzes/:quizId/questions/:questionId" component={App} />
+          <Route exact path="/quizzes/:quizId/edit/" component={App} />
+          <Route exact path="/quizzes/create" component={App} />
+          <Route exact path="/quizzes" component={App} />
+          <Route exact path="/results/statistics/quizzes/:quizId" component={App} />
+          <Route exact path="/results/my/quizzes/:quizId" component={App} />
+          <Route exact path="/results/statistics" component={App} />
+          <Route exact path="/results" component={App} />
+          <Route exact path="/users/:userId" component={App} />
+          <Route exact path="/users" component={App} />
+          <Route exact path="/profile" component={App} />
+          <Route exact path="/register" component={App} />
           <Route exact path="/login" component={Login} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
-          <Route path="/" component={} />
+          <Route exact path="/" component={App} />
         </Switch>
       </BrowserRouter>
     );
