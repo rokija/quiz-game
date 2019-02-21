62fd87011e428fec0d5299a67e06905d96dc4ba8
Elena <kellydj.official@gmail.com>
2019-02-21 11:54:30 +0200

    added quizzes component in quizContainer

--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
+import Quizzes from "../components/Quizzes/Quizzes";
 
+const quizzes = [
+  {
+    title: "First Quiz",
+    description: "Description about quiz",
+    isAdmin: false
+  },
+  {
+    title: "Second Quiz",
+    description: "Description about quiz",
+    isAdmin: true
+  },
+  {
+    title: "Third Quiz",
+    description: "Description about quiz",
+    isAdmin: false
+  },
+  {
+    title: "Fourth Quiz",
+    description: "Description about quiz",
+    isAdmin: true
+  },
+  { title: "Fifth Quiz", description: "Description about quiz", isAdmin: false }
+];
+
+const user = {
+  username: "lalal",
+  email: "emai",
+  hashedPassword: "string",
+  name: "string",
+  surname: "string",
+  dateOfBirth: "2019-02-20",
+  level: "admin",
+  id: 0
+};
 export class QuizContainer extends Component {
   render() {
-    return <div>This is QuizContainer!</div>;
+    return <Quizzes quizzes={quizzes} user={user} />;
   }
 }
 
